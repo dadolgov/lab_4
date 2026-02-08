1077fe2f30e61ea1e19041cd0ff36a53c5d8fc41
Dmitrii Dolgov <ddolgov1@student.cscc.edu>
2026-02-08 12:52:09 -0500

    Added proper output and card limit

--- lab_4.py
+++ lab_4.py
@@ -10,17 +10,25 @@ import random
 card_suits=["c", "h", "s", "d"]
 card_values=["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 deck=[]
+hand=[]
 
 # filling the deck list
 for suit in card_suits:
     for value in card_values:
         deck.append(suit+value)
-print(deck)
 
-# drawing random cards from deck
+# drawing cards
 number_of_cards=int(input("Write the number of cards: "))
-
+# Protection from drawing too many cards
+while(number_of_cards>52):
+    print("Wrong number of cards! Draw between 0 and 52!")
+    number_of_cards=int(input("Write the number of cards: "))
+# filling the player's hand with random cards
 for _ in range(number_of_cards):
-    print(deck.pop(random.randint(0,len(deck)-1)))
+    current_card=random.randint(0,len(deck)-1)
+    hand.append(deck.pop(current_card))
 
-print(deck)
+# Results output
+print("Your cards are:")
+print(*hand, sep=", ")
+print("Total cards in hand: ", len(hand))
