75fbf1f60d0be07dcc50175f0e5faab302e76816
Dmitrii Dolgov <ddolgov1@student.cscc.edu>
2026-02-08 12:18:44 -0500

    Added drawing cards from the deck

--- lab_4.py
+++ lab_4.py
@@ -6,15 +6,21 @@ Date:2/8/2026
 """
 
 import random
-
+# initializing deck 
 card_suits=["c", "h", "s", "d"]
 card_values=["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
-
 deck=[]
 
+# filling the deck list
 for suit in card_suits:
     for value in card_values:
         deck.append(suit+value)
 print(deck)
 
-    
+# drawing random cards from deck
+number_of_cards=int(input("Write the number of cards: "))
+
+for _ in range(number_of_cards):
+    print(deck.pop(random.randint(0,len(deck)-1)))
+
+print(deck)
